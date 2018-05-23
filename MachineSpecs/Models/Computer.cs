59afa78f9c1408ef7ce87778df67af1111125faa
George Shiangoli <gshian01@mail.bbk.ac.uk>
2018-05-23 07:34:14 +0100

    Basic domain model created.

---
+++ MachineSpecs/Models/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MachineSpecs.Models
+{
+    public class Computer
+    {
+        public int ComputerID { get; set; }
+        public int Memory { get; set; }
+        public int StorageCapacity { get; set; }
+        public string StorageType { get; set; }
+        public decimal Weight { get; set; }
+        public int Power { get; set; }
+    }
+}
